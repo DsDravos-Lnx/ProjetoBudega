b3a11798e997b677a748ba008776cf0856f47305
Alex Alves dos Santos <ds.hallucinate@gmail.com>
2019-11-12 19:21:35 -0300

    views started

--- projetoBudega/app/Admin.php
+++ projetoBudega/app/Admin.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     protected $fillable = [
-        'nome', 'email', 'login', 'password', 'created_by', 'update_by',
+        'name', 'email', 'login', 'password',
     ];
 
     protected $table = 'admins';
