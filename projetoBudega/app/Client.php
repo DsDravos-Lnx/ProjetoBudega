5fc5e253ff8be5d4f4162383fc298f3593264522
Alex Alves dos Santos <ds.hallucinate@gmail.com>
2019-11-13 09:50:36 -0300

    relationship commit

--- projetoBudega/app/Client.php
+++ projetoBudega/app/Client.php
@@ -11,4 +11,5 @@ class Client extends Model
     ];
 
     protected $table = 'clients';
+
 }
--- projetoBudega/app/Purshase.php
+++ projetoBudega/app/Purshase.php
@@ -7,13 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Purshase extends Model
 {
     protected $fillable = [
-        'amount', 'price', 'credited',
+        'amount', 'price', 'credited', 'client_id', 'product_id',
     ];
 
     protected $table = 'purshases';
 
+    public function client() {
+        return $this->hasOne(Client::class, 'client_id');
+    }
+
     public function debtors() {
         return Purshase::all()->where('credited', '=', 1);
     }
+
 
 }
